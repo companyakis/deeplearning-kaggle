9afeee2236c5984eed478c90e0148989fb25d4dc
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-27 10:08:27 +0300

    Create p1-image-classification-0001-load-data.py

---
+++ p1-image-classification-0001-load-data.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+
+# load fashion mnist dataset
+fashion_mnist_data = tf.keras.datasets.fashion_mnist.load_data()
+
+print(type(fashion_mnist_data)) #<class 'tuple'>
+
+#print(fashion_mnist_data[0])
