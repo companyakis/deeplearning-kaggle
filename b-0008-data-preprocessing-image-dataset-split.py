e347e64c5a697dac6597800130b35fa7a685f381
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-03 13:21:06 +0300

    Create b-0008-data-preprocessing-image-dataset-split.py

---
+++ b-0008-data-preprocessing-image-dataset-split.py
@@ -0,0 +1,15 @@
+length_flowers_dataset = len(flowers_dataset)
+
+print(length_flowers_dataset) # 3670
+
+train_size = int(length_flowers_dataset * 0.85)
+
+train__flowers_dataset = flowers_dataset.take(train_size)
+
+print(len(train__flowers_dataset))
+
+test_flowers_dataset = flowers_dataset.skip(train_size)
+
+print(len(test_flowers_dataset))
+
+print(3119 + 551) # 3670
